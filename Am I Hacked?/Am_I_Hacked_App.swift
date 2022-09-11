6d3751765c1f72d66cf5f96e8d1063bd4e07b60a
Prokhorov Roman <roman.prokhorov@raiffeisen.ru>
2022-09-11 17:28:40 +0300

    Initial Commit

---
+++ Am I Hacked?/Am_I_Hacked_App.swift
@@ -0,0 +1,17 @@
+//
+//  Am_I_Hacked_App.swift
+//  Am I Hacked?
+//
+//  Created by PROKHOROV Roman on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Am_I_Hacked_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
